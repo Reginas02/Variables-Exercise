a42e2526646bba472dac8fd9eafec630f0df0d5c
Regina Naslund <rnaslund66@gmail.com>
2022-10-20 07:43:58 -0700

    variables finished

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace VariablesExercise
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿// Declare and initialize variables
+string dogName = "MoJo";
+int age = '3';
+char lastInitial = 'N';
+bool alwaysNeedsAttention = true;
+double currentWieght = 120.5;
+decimal goalWeight = 125;
+
+Console.WriteLine($"dogName: {"dogName"}");
+Console.WriteLine($"age: {age}");
+Console.WriteLine($"lastInitial: {"lastInitial"}");
+Console.WriteLine($"currentWeight: {"currentWeight"}");
+Console.WriteLine($"goalWeight: {goalWeight}");
+Console.WriteLine($"alwaysNeedsAttention: {alwaysNeedsAttention}");
+Console.WriteLine($"My dog's name is {dogName} {lastInitial} he is {age}years old.");
+
+
